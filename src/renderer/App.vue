462585844f9789abb97bd643ca62d21557cee45b
devyyj <ryong0906@naver.com>
2020-08-16 11:53:47 +0900

    초기 작업

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,78 +5,81 @@
 </template>
 
 <script>
-  import Board from './Board'
-  import Block from './Block'
+import Board from "./board";
+import Block from "./block";
 
-  // 가로 세로 픽셀
-  const PIXEL = 640
-  // 한줄에 생성될 블럭 수
-  const BOLCK_COUNT = 16
-  // 정사각형이라 가로 세로를 따로 설정할 필요는 없지만,
-  // 나중에 정사각형이 아닌 형태를 만들수 있도록 가로/세로 변수를 설정.
-  const COLS = BOLCK_COUNT
-  const ROWS = BOLCK_COUNT
-  // 블럭 사이즈, 하나의 블럭이 이루는 픽셀
-  const BLOCK_SIZE = PIXEL / BOLCK_COUNT
-
-  let canvas
-  let ctx
-  let board
-
-  export default {
-    name: 'wearehere',
-    data() {
-      return {}
-    },
-    mounted() {
-      this.init()
-    },
-    methods: {
-      init() {
-        canvas = document.getElementById('board')
-        ctx = canvas.getContext('2d')
-
-        // 상수를 사용해 캔버스의 크기를 계산한다.
-        // 선을 그릴때 좌표에 0.5픽셀씩 더하기 때문에 +1을 해준다.
-        ctx.canvas.width = COLS * BLOCK_SIZE + 1
-        ctx.canvas.height = ROWS * BLOCK_SIZE + 1
-
-        // 선을 그린다.
-        this.drawGrid(PIXEL, PIXEL)
-
-        // 블록의 크기를 변경한다.
-        ctx.scale(BLOCK_SIZE, BLOCK_SIZE)
-
-        // 보드 생성
-        board = new Board(COLS, ROWS)
-        board.reset()
-        console.table(board.grid)
-
-        // 블럭 그리기
-        let block = new Block(ctx)
-        block.draw()
-
-        board.block = block
+export default {
+  name: "wearehere",
+  data() {
+    return {
+      canvas: undefined,
+      ctx: undefined,
+      board: undefined,
+      config: {
+        width: 800,
+        height: 800,
+        cols: 20,
+        rows: 20,
       },
-      drawGrid(bw, bh) {
-        // 왜 0.5를 더해야 선명한 선이 그려지는 지 모르겠음.
-        // 세로 선
-        for (var x = 0; x <= bw; x += BLOCK_SIZE) {
-          ctx.moveTo(0.5 + x, 0)
-          ctx.lineTo(0.5 + x, bh)
-        }
-        // 가로 선
-        for (var x = 0; x <= bh; x += BLOCK_SIZE) {
-          ctx.moveTo(0, 0.5 + x)
-          ctx.lineTo(bw, 0.5 + x)
-        }
-        ctx.strokeStyle = 'black'
-        ctx.stroke()
-      },
+    };
+  },
+  computed: {
+    blockWidthSize() {
+      return this.config.width / this.config.cols;
     },
-  }
+    blockHeightSize() {
+      return this.config.height / this.config.rows;
+    },
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.canvas = document.getElementById("board");
+      this.ctx = this.canvas.getContext("2d");
+
+      // 상수를 사용해 캔버스의 크기를 계산한다.
+      // 선을 그릴때 좌표에 0.5픽셀씩 더하기 때문에 +1을 해준다.
+      this.ctx.canvas.width = this.config.width + 1;
+      this.ctx.canvas.height = this.config.height + 1;
+
+      // 선을 그린다.
+      this.drawGrid(this.config.width, this.config.height);
+
+      // 블록의 크기를 변경한다.
+      this.ctx.scale(this.blockWidthSize, this.blockHeightSize);
+
+      // 보드 생성
+      this.board = new Board(this.config.rows, this.config.cols);
+      this.board.reset();
+      console.table(this.board.grid);
+
+      // 블럭 그리기
+      let block = new Block(this.ctx);
+      block.draw();
+
+      this.board.block = block;
+    },
+    drawGrid(width, height) {
+      // 왜 0.5를 더해야 선명한 선이 그려지는 지 모르겠음.
+      // 세로 선
+      for (let x = 0; x <= width; x += this.blockWidthSize) {
+        this.ctx.moveTo(0.5 + x, 0);
+        this.ctx.lineTo(0.5 + x, height);
+      }
+      // 가로 선
+      for (let y = 0; y <= height; y += this.blockHeightSize) {
+        this.ctx.moveTo(0, 0.5 + y);
+        this.ctx.lineTo(width, 0.5 + y);
+      }
+      this.ctx.strokeStyle = "black";
+      this.ctx.stroke();
+    },
+  },
+};
 </script>
 
 <style>
-  /* CSS */
+/* CSS */
 </style>
--- src/renderer/Board.js
+++ src/renderer/Board.js
@@ -1,20 +1,20 @@
 class Board {
-  constructor(ROWS, COLS) {
-    this.grid
-    this.ROWS = ROWS
-    this.COLS = COLS
+  constructor(rows, cols) {
+    this.grid;
+    this.rows = rows;
+    this.cols = cols;
   }
 
   // 새 게임이 시작되면 보드를 초기화한다.
   reset() {
-    this.grid = this.getEmptyBoard()
+    this.grid = this.getEmptyBoard();
   }
 
   // 0으로 채워진 행렬을 얻는다.
   getEmptyBoard() {
-      console.log(this.ROWS);
-    return Array.from({length: this.ROWS}, () => Array(this.COLS).fill(0))
+    console.log(this.rows);
+    return Array.from({ length: this.rows }, () => Array(this.cols).fill(0));
   }
 }
 
-export default Board
+export default Board;
